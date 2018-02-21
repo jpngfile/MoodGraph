7d4ae434e3b0548d6fc309bd070ac10b1595eb0a
Jason P'ng <jsjpng@gmail.com>
2018-02-20 19:48:15 -0800

    Added user creation to controller

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,8 +1,33 @@
+var User = require('../models/user');
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 exports.account_create_get = function(req, res) {
     res.render('signup', { title: "Signup"});
 }
 
-exports.account_create_post = function(req, res) {
-    res.send("Account creation not implemented")
-}
+exports.account_create_post = [
+    body('username').isLength({ min: 1}).trim().withMessage('Username must be specified').isAlphanumeric().withMessage('Username has non-alphanumeric characters.'),
+    body('password').isLength({ min: 1}).withMessage('Password must be specified'),
+
+    sanitizeBody('username').trim().escape(),
+    sanitizeBody('password').escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.render('signup', { title: "Signup", user: req.body, errors: errors.array() });
+            return;
+        } else {
+            var user = new User({
+                username: req.body.username,
+                password: req.body.password,
+                years: []
+            });
+            user.save(function (err) {
+                if (err) { return next(err); }
+                res.send("user created");
+            })
+        }
+    }
+];
--- models/user.js
+++ models/user.js
@@ -4,14 +4,14 @@ var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
     username: {type: String, required: true, max: 100},
-    password: {type: String, required: true, max 100},
+    password: {type: String, required: true, max: 100},
     years: [{ type: Schema.ObjectId, ref: 'Year' }],
 });
 
 UserSchema
 .virtual('url')
 .get(function () {
-    reuturn '/users/' + this._id;
+    return '/users/' + this._id;
 });
 
 module.exports = mongoose.model('User', UserSchema);
