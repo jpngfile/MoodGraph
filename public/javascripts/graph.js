bee5d190bcdb7c3722d272ab70bfb43a2867f170
Jason P'ng <jsjpng@gmail.com>
2018-02-27 21:11:10 -0800

    Added preliminary d3 testing changes

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -1,1 +1,19 @@
 console.log(user);
+var CELL_SIZE = 50;
+var width = 100
+    heigth = 100
+    cellSize = 15
+
+d3.select('.heatmap')
+    .append("svg")
+    .append("rect")
+    .attr('width', CELL_SIZE)
+    .attr('height', CELL_SIZE)
+    .attr('x', 0)
+    .attr('y', 0)
+    .style('fill', 'blue')
+    
+d3.select("body")
+  .style('background-color', 'red')
+  .append('p')
+  .text("sample text")
