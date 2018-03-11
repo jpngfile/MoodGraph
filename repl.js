aec742e1232005207e7a3fd18319c1b3b0566438
Jason P'ng <jsjpng@gmail.com>
2018-03-10 21:57:52 -0800

    Added basic repl connected to database'

---
+++ repl.js
@@ -0,0 +1,17 @@
+var net = require("net");
+var repl = require('repl');
+var mongoose = require('mongoose');
+//connections = 0;
+//
+//net.createServer(function (socket) {
+//    connections += 1;
+//    repl.start("Node via TCP socket", socket);
+//}).listen(3000);
+var mongoDB = 'mongodb://localhost/test'
+mongoose.connect(mongoDB, {});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+var shell = repl.start("node>");
+shell.context.mongoose = mongoose;
+shell.context.db = db;
+
