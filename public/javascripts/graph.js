9155f3703b5b16099bc15966da47ee3db6bcbcd2
Jason P'ng <jsjpng@gmail.com>
2018-03-12 20:58:40 -0700

    Changed user detail to show all years

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -2,8 +2,8 @@ console.log(user);
 var CELL_SIZE = 15;
 var buffer = 3;
 
-var year = user.years[0]
-var date = year.days[0]
+//var year = user.years[0]
+//var date = year.days[0]
 var colourMap = new Map([
     ['unassigned', 'blue'],
     ['happy', 'yellow'],
@@ -11,27 +11,30 @@ var colourMap = new Map([
     ['neutral', 'black']
 ]);
 
-var svg = d3.select('.heatmap')
-    .append("svg")
-    .attr('width', 1000)
+for(var i = 0; i < user.years.length; i++){
+    var year = user.years[i];
+    var svg = d3.select('.heatmap')
+        .append("svg")
+        .attr('width', 1000)
+        
+    var g = svg.append('g')
+        
+    var rects = g.selectAll('rect')
+        .data(year.days)
     
-var g = svg.append('g')
+    //rects.exit().remove()
     
-var rects = g.selectAll('rect')
-    .data(user.years[0].days)
-
-rects.exit().remove()
-
-rects.enter().append("rect")
-    .attr('width', CELL_SIZE)
-    .attr('height', CELL_SIZE)
-    .attr('x', (d) => d3.timeFormat('%U')(new Date(d.date)) * (CELL_SIZE + buffer))
-    .attr('y', (d) => new Date(d.date).getDay() * (CELL_SIZE + buffer))
-    .text(function (d) {
-        var date = new Date(d.date)
-        return d3.timeFormat('%U')(date)    
-    })
-    .style('fill', (d) => colourMap.get(d.mood))
+    rects.enter().append("rect")
+        .attr('width', CELL_SIZE)
+        .attr('height', CELL_SIZE)
+        .attr('x', (d) => d3.timeFormat('%U')(new Date(d.date)) * (CELL_SIZE + buffer))
+        .attr('y', (d) => new Date(d.date).getDay() * (CELL_SIZE + buffer))
+        .text(function (d) {
+            var date = new Date(d.date)
+            return d3.timeFormat('%U')(date)    
+        })
+        .style('fill', (d) => colourMap.get(d.mood))
+}
 //    .style('stroke-width', 3)
 //    .style('stroke', 'black')
     
