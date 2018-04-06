b15637c5c7f9b69f7420dbd6eaf75955480acffd
Jason P'ng <jsjpng@gmail.com>
2018-04-05 19:26:03 -0700

    Removed unused users route

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -44,7 +43,6 @@ app.use(function(req, res, next) {
 });
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
