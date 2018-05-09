2478e5717063ddd8198360a943678fe9b741be17
Jason P'ng <jsjpng@gmail.com>
2018-05-08 21:35:43 -0400

    Added moods to user model

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ var UserSchema = new Schema({
     username: {type: String, unique: true, required: true, max: 100},
     password: {type: String, required: true, max: 100},
     years: [{ type: Schema.ObjectId, ref: 'Year' }],
+    moods: [{ type: Schema.ObjectId, ref: 'Mood' }],
     created: {type: Date, default: Date.now()},
 });
 
