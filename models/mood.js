9b5c59839cfc098e3c5c6759f8dadd7665828e0d
Jason P'ng <jsjpng@gmail.com>
2018-05-09 22:17:56 -0400

    Added image field to moods

--- models/mood.js
+++ models/mood.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 
 var MoodSchema = new Schema({
     mood: {type: String, required: true, max: 100},
-    color: {type: String, required: true}
+    color: {type: String, required: true},
+    imagePath: {type: String}
 })
 
 module.exports = mongoose.model('Mood', MoodSchema);
